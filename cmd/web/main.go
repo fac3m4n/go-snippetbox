b2c1566fcaf3b24613573f39b1b6265122711872
fac3m4n <akerimberdi@gmail.com>
2024-10-04 14:33:39 +0300

    isolate routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,19 +21,10 @@ func main() {
 
 	app := &application{logger: logger}
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-
 	logger.Info("starting server", "addr", *addr)
+	app.routes()
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 
