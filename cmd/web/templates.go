5c08f009ae783a3968151dd7d5f26defc404066c
fac3m4n <akerimberdi@gmail.com>
2024-10-07 15:15:36 +0300

    adding humanDate func

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -15,6 +15,17 @@ type templateData struct {
 	CurrentYear int
 }
 
+func humanDate(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
@@ -23,7 +34,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	}
 	for _, page := range pages {
 		name := filepath.Base(page)
-		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
