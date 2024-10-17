bf48ebd208474f738871ea4054aaaf0b43958faa
fac3m4n <akerimberdi@gmail.com>
2024-10-17 11:12:33 +0300

    adding logger middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -12,6 +13,19 @@ func commonHeaders(next http.Handler) http.Handler {
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "0")
 		w.Header().Set("Server", "Go")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ip     = r.RemoteAddr
+			proto  = r.Proto
+			method = r.Method
+			uri    = r.URL.RequestURI()
+		)
+		app.logger.Info(fmt.Sprintf("%s - %s %s %s", ip, proto, method, uri))
 		next.ServeHTTP(w, r)
 	})
 }
